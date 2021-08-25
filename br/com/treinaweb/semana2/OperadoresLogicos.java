633a2844bd392698e714ed37e4aa0c215c1c2e1e
Cleyson Lima <cleysonph@gmail.com>
2021-08-24 23:24:38 -0300

    02:12 - operadores logicos

---
+++ br/com/treinaweb/semana2/OperadoresLogicos.java
@@ -0,0 +1,14 @@
+package br.com.treinaweb.semana2;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        // Not | Não
+        System.out.println(!(10 != 10));
+
+        // And | E
+        System.out.println( 10 > 20 && 10 < 5 );
+
+        // Or | Ou
+        System.out.println(10 > 20 || 10 < 5);
+    }
+}
