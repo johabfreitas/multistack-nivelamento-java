35258f6ed0813398b0113b5ee4fcef2cc213d408
Cleyson Lima <cleysonph@gmail.com>
2021-08-24 22:57:39 -0300

    02:10 - operadores matematicos

--- br/com/treinaweb/semana2/OlaMundo.java
+++ br/com/treinaweb/semana2/OlaMundo.java
@@ -2,23 +2,6 @@ package br.com.treinaweb.semana2;
 
 public class OlaMundo {
     public static void main(String[] args) {
-        String nome = "TreinaWeb";
-        int idade = 20;
-        float altura = 1.7f;
-        double altura2 = 1.7;
-        boolean estaVivo = true;
-
-        var variavel = "TreinaWeb com var";
-
-        System.out.println(nome);
-        System.out.println(idade);
-        System.out.println(altura);
-        System.out.println(altura2);
-        System.out.println(estaVivo);
-        System.out.println(variavel);
-
-        final var CONSTANTE = "TreinaWeb";
-
-        System.out.println(CONSTANTE);
+        System.out.println("Olá Mundo");
     }
 }
---
+++ br/com/treinaweb/semana2/OperadoresMatematicos.java
@@ -0,0 +1,30 @@
+package br.com.treinaweb.semana2;
+
+public class OperadoresMatematicos {
+    public static void main(String[] args) {
+        // Soma
+        var soma = 10 + 10;
+
+        System.out.println(soma);
+
+        // Subtração
+        var subtracao = 10 - 10;
+
+        System.out.println(subtracao);
+
+        // Multiplacação
+        var multiplicacao = 10 * 10;
+
+        System.out.println(multiplicacao);
+
+        // Divisão
+        var divisao = 3 / 2.0;
+
+        System.out.println(divisao);
+
+        // Modulo
+        var modulo = 3 % 2;
+
+        System.out.println(modulo);
+    }
+}
